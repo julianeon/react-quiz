60f8559b69010413d0286e7e4c40db3e00a0c325
julianeon <julianeon@gmail.com>
2019-06-28 14:56:19 -0700

    Working.

--- README.md
+++ README.md
@@ -1,68 +1,18 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project was made with React, Create React App, and this helpful reference, which I liberally copied from (it's most of the code, by volume, so to speak).
 
-## Available Scripts
+https://codepen.io/Daanist/pen/LjLoWV
 
-In the project directory, you can run:
+## Comments
 
-### `npm start`
+I wanted to make the simplest possible Quiz app using React that you could make. I wasn't able to find a simpler one than this - so I made it.
 
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+You can run and edit here on Glitch, or clone it from GitHub, run yarn install and yarn start, and then deploy to AWS Amplify (though lol, who would do that for this).
 
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
+### Improvements
 
-### `npm test`
+I could definitely have made the intro and outro screens snazzier.
 
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+You'll notice there's a whole state variable - correct - I'm not even using. You could.
 
-### `npm run build`
+Most obviously, you can edit the questions to make your own quiz, or pick superior gifs.
 
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
--- src/App.js
+++ src/App.js
@@ -1,26 +1,200 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import garbage from './garbage.gif';
+import up from './yesss.gif';
+import down from './thomas.gif';
+import { Route, Link, HashRouter as Router } from 'react-router-dom';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Center = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  /* bring your own prefixes */
+  transform: translate(-50%, -50%);
+  text-align: center;
+`
+
+const StyledLink = styled(Link)`
+    text-decoration: none;
+
+    &:focus, &:hover, &:visited, &:link, &:active {
+        text-decoration: none;
+    }
+`
+
+ const dataSet = [
+      {
+        question: "Puppies",
+        answers: [
+          "Good",
+          "Bad"
+        ],
+        correct: 0
+      },
+      {
+        question: "Evil",
+            answers: [
+		"Good",
+		"Bad"
+            ],
+            correct: 1
+      },
+       {
+            question: "... children.",
+            answers: [
+              "Be good to",
+              "Be bad to"
+            ],
+            correct: 0
+          },
+          {
+            question: "As President, how many concentration camps should you build?",
+            answers: [
+		"0",
+		"100",
+		"As many as a billion dollars will buy",
+		"One for every American"
+            ],
+            correct: 0
+          },
+    ];
+
+
+class App extends Component {
+  render() {
+    return(
+	    <div>
+	    	  <Router>
+          <div className="content">
+          <Route exact path="/" component={Intro}/>
+ 	  <Route path="/quiz" component={Quiz} />
+ 	  <Route path="/finish" component={Outro} />	  
+	  </div>
+	  </Router>
+
+      </div>
+    )
+  }
+}
+
+const Intro = () => {
+    return (
+	    <Center>
+	    <h1>Are you a garbage person?</h1>
+	    <img src={garbage} alt="garbage"/>
+	    <p>Take this one weird quiz and find out!</p>
+	    <StyledLink to="/quiz" component={Quiz}>&#9193;</StyledLink>	    
+	    </Center>
+    )
+}
+
+function Outro(props) {
+    if (props.score===0) {
+    return (
+	    <Center>
+	    <h1>Verdict</h1>
+	    <img src={up} alt="good"/>
+	    </Center>
+    )
+    } else
+    return (
+	    <Center>
+	    <h1>Verdict</h1>
+	    <img src={down} alt="garbage"/>
+	    </Center>
+    )
+	
+}
+
+class Quiz extends Component {
+    constructor(props) {
+    super(props)
+    
+    this.state = {current:0, dataSet:dataSet, correct:0, incorrect:0}
+    this.handleClick = this.handleClick.bind(this)
+    
+  } 
+  
+  handleClick(choice) {
+    if (choice === this.state.dataSet[this.state.current].correct) {
+	this.setState({correct: this.state.correct + 1})
+    } else {
+	this.setState({incorrect: this.state.incorrect + 1})
+    }
+    
+    if (this.state.current < dataSet.length) {
+	this.setState({current: this.state.current + 1}) 
+    }}
+
+    render() {
+	if (this.state.current === dataSet.length) {
+            return (
+		    <div>
+		    <Outro score={this.state.incorrect}/>
+		    </div>
+	    )
+	} else {
+  	    console.log(this.state.current); 
+	    return (
+		    <div>
+		    <QuizArea handleClick={this.handleClick} dataSet={this.state.dataSet[this.state.current]} />
+		    </div>
+	    )
+	}}
+}
+
+function QuizArea(props) {
+  var style = {
+    width: "25%",
+    display: "block",
+    textAlign: "center",
+    boxSizing: "border-box",
+    float: "left",
+    padding: "0 2em"
+  }
+  return(
+    <div style={style}>
+      <Question dataSet={props.dataSet} />
+      <AnswerList dataSet={props.dataSet} handleClick={props.handleClick} />
     </div>
-  );
+  )
+}
+
+
+
+function Question(props) {
+  var style = {
+    color: "red",
+  }
+  return (
+    <h1 style={style}>{props.dataSet.question}</h1>
+  )
+}
+
+function Answer(props) {
+  var style = {
+    width: "100%",
+    height: 50,
+    color: "blue"
+  }
+  return(
+    <div>
+      <button style={style} onClick={() => props.handleClick(props.choice)}>{props.answer}</button>
+    </div>
+  )
+}
+
+function AnswerList(props) {
+  var answers = []
+  for (let i = 0; i < props.dataSet.answers.length; i++) {
+    answers.push(<Answer choice={i} handleClick={props.handleClick} answer={props.dataSet.answers[i]} />)
+  }
+  return(
+    <div>
+      {answers}
+    </div>
+  )
 }
 
 export default App;
